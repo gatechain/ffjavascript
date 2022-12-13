88f2d484d6d04f51049bf529f9d191072e601871
Thone Liu <thorne_lew@126.com>
2022-12-13 14:37:37 +0800

    init

---
+++ src/scalar.js
@@ -0,0 +1,8 @@
+// const supportsNativeBigInt = typeof BigInt === "function";
+// if (supportsNativeBigInt) {
+//     module.exports = require("./scalar_native.js");
+// } else {
+//     module.exports = require("./scalar_bigint.js");
+// }
+
+module.exports = require("./scalar_bigint.js");
